021b10c0f86c21385f4002b11ec551de11dba1cb
Bruna Gomes <bruhbg55@gmail.com>
2025-12-01 20:13:46 -0300

    feat: criar endpoint para enviar mensagens para o canal 'chat-geral' no Ably

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,17 +8,27 @@ use Ably\AblyRest;
 
 class ChatController extends Controller
 {
+    /**
+     * Envia uma mensagem para o canal 'chat-geral' no Ably
+     */
     public function enviar(Request $request)
     {
-        $dados = $request->all();
+        $dados = $request->validate([
+            'usuario' => 'required|string',
+            'texto'   => 'required|string',
+            'hora'    => 'required|string',
+        ]);
 
-        Log::info('Enviando para Ably:', $dados);
+        Log::info('Enviando mensagem para Ably:', $dados);
 
-        // Publicar no Ably
         $ably = new AblyRest(env('ABLY_KEY'));
         $canal = $ably->channels->get('chat-geral');
+
         $canal->publish('nova-mensagem', $dados);
 
-        return response()->json(['ok' => true]);
+        return response()->json([
+            'ok' => true,
+            'mensagem' => $dados
+        ]);
     }
 }
